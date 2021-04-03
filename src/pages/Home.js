676fc4cc2d4a40cfb8aaa8b4a3668c9c7af4d1e9
YoonSoo <fhkaldh@naver.com>
2021-04-04 04:28:37 +0900

    init and Home

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "stylesheets/Home.css";
+
+const Home = () => {
+    return (
+        <div className="container__home">
+            <header className="header__home">Welcome</header>
+        </div>
+    );
+};
+
+export default Home;
